da764403c33f6aa9ef3a64815064edb4a5f7b391
MichaelNeum <56724734+MichaelNeum@users.noreply.github.com>
2020-12-07 16:59:09 +0100

    added haarcascade xml

--- src/Cameraman.py
+++ src/Cameraman.py
@@ -1,4 +1,14 @@
 import cv2 as cv
 import sys
 
-print("Hello World!")
+cascPath = sys.argv[1]
+faceCascade = cv.CascadeClassifier(cascPath)
+
+videoCapture = cv.VideoCapture(0)
+
+while True:
+    ret, frame = videoCapture.read()
+
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    cv.imshow('Video', frame)
